37b48d4fb680b525e2279bc02c0fd302d3588095
jones2000 <jones_2000@163.com>
2020-05-04 12:00:18 +0800

    ver 8413 py 修改测试用例

--- umychart_python/umychart_cache_testcase.py
+++ umychart_python/umychart_cache_testcase.py
@@ -9,6 +9,8 @@
 
 import sys
 import codecs
+import time
+import numpy as np
 from umychart_complier_jscomplier import JSComplier, SymbolOption, HQ_DATA_TYPE
 from umychart_complier_data import PERIOD_ID
 from umychart_complier_jscomplier import ScriptIndexConsole, ScriptIndexItem, SymbolOption, RequestOption, HQ_DATA_TYPE, ArgumentItem
@@ -16,80 +18,146 @@ from umychart_complier_pandas_help import JSComplierPandasHelper
 from umychart_complier_jssymboldata import JSSymbolData
 from umychart_complier_jssymboldata_cache import SymbolDataCache
 
+# 单个股票的数据都放这里, 脚本执行的时候直接读这里的数据
+class CacheItem():
+    def __init__(self, symbol) :
+        self.Symbol=symbol
+        self.KData=None # K线数据
+
+    # 加载数据
+    def Load(self):
+        data = np.array([
+            (20200326, 3.5, 3.51, 3.53, 3.49, 85418999.0),
+            (20200327, 3.53, 3.52, 3.54, 3.51, 70252329.0),
+            (20200330, 3.51, 3.52, 3.52, 3.5, 81439044.0),
+            (20200331, 3.53, 3.48, 3.54, 3.48, 69618442.0),
+            (20200401, 3.49, 3.48, 3.51, 3.48, 66160346.0),
+            (20200402, 3.47, 3.5, 3.5, 3.46, 74938267.0),
+            (20200403, 3.49, 3.49, 3.5, 3.48, 56193251.0),
+            (20200407, 3.52, 3.51, 3.53, 3.5, 82141037.0),
+            (20200408, 3.5, 3.49, 3.5, 3.48, 55274495.0),
+            (20200409, 3.5, 3.49, 3.5, 3.49, 35858062.0)])
+        self.KData=data
 
 
-# 加载自己的外部数据
-def LoadCacheData(symbolData):
-    print("[LoadCacheData] start load [{0},Period={1},Right={2}] cache to hqchart".format(symbolData.Symbol, symbolData.Period, symbolData.Right))
+# 管理所有的股票缓存数据
+class CacheData:
+    Cache={} # 数据缓存
 
-    # 对应股票的K线数据 (必须设置)
-    symbolData.Data.Data['CLOSE']=[10, 11, 10.3, 11.2, 11.3, 10.3, 10.4]
-    symbolData.Data.Data['DATE']=[20200413, 20200414, 20200415, 20200416, 20200417, 20200418, 20200419]
+    @staticmethod
+    def Load():
+       arySymbol=['600000.sh','000001.sz']
+       for item in arySymbol:
+           stock=CacheItem(item)
+           stock.Load()
+           CacheData.Cache[item]=stock
 
-    # 财务数据
-    #FINANCE(7)
-    symbolData.FinanceData[7]=[110, 121, 140.3, 161.2, 181.3, 110.3, 120.4]
+    @staticmethod
+    def LoadCacheData(symbolData) : # 加载自己的外部数据
+        print("[CacheData::LoadCacheData] start load [{0},Period={1},Right={2}] cache to hqchart".format(symbolData.Symbol, symbolData.Period, symbolData.Right))
+        if (not symbolData.Symbol in CacheData.Cache.keys()) :
+            # 报错缓存里面没这个股票数据
+            return 
 
-    # 大盘数据 数据要和对应这个股票的日期对应上
-    symbolData.IndexData.Data["CLOSE"]=[210, 211, 210.3, 211.2, 211.3, 210.3, 210.4]
+        stock=CacheData.Cache[symbolData.Symbol]
+        kData=stock.KData
+        aryDate=list(kData[:, 0]) # !! 有问题 怎么是小数的 要转成int才可以
+        aryOpen = list(kData[:, 1])
+        aryHigh = list(kData[:, 3])
+        aryLow = list(kData[:, 4])
+        aryClose = list(kData[:, 2])
+        aryVol = list(kData[:, 5])
 
-    # 最新行情
-    # DYNAINFO(4) 
-    symbolData.LatestKCache[4]=15
-    pass
+        # 对应股票的K线数据 (必须设置)
+        symbolData.Data.Data['CLOSE']=aryClose
+        symbolData.Data.Data['DATE']=aryDate
+        symbolData.Data.Data['VOL']=aryVol
+        symbolData.Data.Data['LOW']=aryLow
+        symbolData.Data.Data['HIGH']=aryHigh
+        symbolData.Data.Data['OPEN']=aryOpen
 
-def ToScprit(code):
-    scprit=""
-    for item in code:
-        scprit+=item
-        scprit+='\n'
-    return scprit
+        # 财务数据
+        #FINANCE(7)
+        symbolData.FinanceData[7]=[110, 121, 140.3, 161.2, 181.3, 110.3, 120.4]
+
+        # 大盘数据 数据要和对应这个股票的日期对应上
+        symbolData.IndexData.Data["CLOSE"]=[210, 211, 210.3, 211.2, 211.3, 210.3, 210.4]
+
+        # 最新行情
+        # DYNAINFO(4) 
+        symbolData.LatestKCache[4]=15  # 最新行情单值
+
+        pass
+
+
+
+
+MACD_INDEX = '''
+DIF:=EMA(C,12)-EMA(C,26);
+DEA:=EMA(DIF,9);
+MACD:(DIF-DEA)*2;
+'''
+
+TEST_INDEX = '''
+T17:CURRBARSCOUNT;
+T16:PEAKBARS(0,5,1);
+T15:TROUGHBARS(2,5,2);
+T14:ZIG(3,5);
+T13:COSTEX(CLOSE,REF(CLOSE,1));
+T12:PPART(10);
+T11:COST(10);
+T0:WINNER(CLOSE);
+T1:INDEXC;
+T3:FINANCE(7);
+T2:C;
+T4:DYNAINFO(4);
+T5:-5;
+T6:-C;
+'''
 
 def RunTestCase():
 
-    SymbolDataCache.LoadCallback=LoadCacheData
+    SymbolDataCache.LoadCallback=CacheData.LoadCacheData  # 设置第3放数据加载回调
 
     name='测试脚本'
 
     # 指标脚本代码
-    code=[
-        "T1:INDEXC;",
-        "T3:FINANCE(7);",
-        "T2:C;",
-        "T4:DYNAINFO(4);"
-    ]
-    script=ToScprit(code)
+    script=MACD_INDEX
 
     # 参数
     args=[ ArgumentItem(name='M1', value=5), ArgumentItem(name='M2', value=10), ArgumentItem(name='M3', value=20)] 
 
     scpritInfo=ScriptIndexItem(name=name, id=888888, script=script, args=args)
     indexConsole = ScriptIndexConsole(scpritInfo)
-    
-    option = SymbolOption(
-        symbol='600000.sh',
-        right=1, # 复权 0 不复权 1 前复权 2 后复权
-        period=PERIOD_ID.DAY, # 周期
-        request=RequestOption(maxDataCount=500,maxMinuteDayCount=3)
-        )
 
-    # 替换成自己的数据类
-    option.ProcCreateSymbolData=SymbolDataCache.Create  
-    result=indexConsole.ExecuteScript(option,False)
+    arySymbol=['600000.sh','000001.sz'] # 要执行策略的股票池
+    aryResult=[] # 结果数据
 
-    # 使用缓存的AST执行
-    option = SymbolOption(
-        symbol='000001.sz',
-        right=1, # 复权 0 不复权 1 前复权 2 后复权
-        period=PERIOD_ID.DAY, # 周期
-        request=RequestOption(maxDataCount=500,maxMinuteDayCount=3)
-        )
-    option.ProcCreateSymbolData=SymbolDataCache.Create  
-    result=indexConsole.ExecuteScript(option,False)
+    runTime=time.time()
 
-    return result
+    for item in arySymbol :
+        startTime = time.time()
+        option = SymbolOption(
+            symbol=item,
+            right=1, # 复权 0 不复权 1 前复权 2 后复权
+            period=PERIOD_ID.DAY, # 周期
+            request=RequestOption(maxDataCount=500,maxMinuteDayCount=3)
+            )
+
+        # 替换成自己的数据类
+        option.ProcCreateSymbolData=SymbolDataCache.Create  
+        result=indexConsole.ExecuteScript(option,False) # 使用缓存的AST执行
+        endTime = time.time()
+
+        print("[RunTestCase] 股票:{0} 耗时:{1}".format(item, endTime - startTime))
+
+        aryResult.append(result)
+
+    print("[RunTestCase] 股票个数:{0} 耗时:{1}".format(len(arySymbol), time.time() - runTime))
+
+    return aryResult
 
 
-
+CacheData.Load()    # 加载股票数据到内存
 
 RunTestCase()
