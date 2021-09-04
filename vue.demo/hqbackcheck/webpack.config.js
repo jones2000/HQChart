999742ffb6648120282445bc298d7984b6f79c78
jones2000 <jones_2000@163.com>
2021-09-05 03:24:06 +0800

    更新demo hqbackcheck依赖包

--- vue.demo/hqbackcheck/webpack.config.js
+++ vue.demo/hqbackcheck/webpack.config.js
@@ -87,10 +87,10 @@ module.exports = (options = {}) => ({
   },
   devServer: {
     host: '127.0.0.1',
-    port: 8005,
+    port: 8080,
     proxy: {
       '/api/': {
-        target: 'http://127.0.0.1:8005',
+        target: 'http://127.0.0.1:8080',
         changeOrigin: true,
         pathRewrite: {
           '^/api': ''
